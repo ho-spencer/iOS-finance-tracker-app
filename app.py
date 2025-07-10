946d921f0972bb73d505fb85727de2047e8fa7cc
Spencer Ho <bspencer08@cox.net>
2025-07-10 16:34:59 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+## ------- SERVER URL: http://127.0.0.1:5000/ 
+app = Flask(__name__)       # create a Flask app instance
+
+
+# Route to appURL/ping to test the server
+@app.route('/ping', methods = ["GET"])
+def pint():
+    return "PING WORKS"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
